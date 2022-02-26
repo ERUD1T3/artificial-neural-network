6e2ec2358e89d3adb9b484c3ae59d306c32126c8
Technocracy <josiasmoukpe@gmail.com>
2022-02-26 09:24:34 -0500

    getting started

--- source/ann.py
+++ source/ann.py
@@ -5,3 +5,26 @@
 #   file: ann.py
 #   Description: main class for artificial neural network
 #############################################################
+
+class ANN:
+    '''
+    Feed Forward Artificial Neural Network Class
+    1 Input, 1 Hidden, 1 Output Layer
+    '''
+    def __init__(
+        self, 
+        num_input, 
+        num_hidden, 
+        num_ouptut, 
+        lr, 
+        momentum, 
+        debug=True
+    ) -> None:
+        
+        self.num_input = num_input
+        self.num_hidden = num_hidden
+        self.num_output = num_ouptut
+        self.lr = lr
+        self.momentum = momentum
+        self.debug = debug
+        
